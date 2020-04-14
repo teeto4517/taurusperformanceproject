893ab57d8583b38b7c84da5a5f8f85608f61b44e
Oluwatoye Owojaye <teeto45@gmail.com>
2020-04-14 04:39:11 +0100

    updated env variables

--- src/main/java/frameWork/basePage.java
+++ src/main/java/frameWork/basePage.java
@@ -3,13 +3,21 @@ package frameWork;
 import Pages.homePage;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.ITestResult;
+
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class basePage {
 
     static WebDriver driver;
+    static Method method;
+    static ITestResult result;
 
     public static homePage executeDriver(String url) {
         driver.get (url);
@@ -17,19 +25,33 @@ public class basePage {
     }
 
     @BeforeClass
-    public static void setDriver() {
-        System.setProperty ( "webdriver.chrome.driver" , "C:/Users/User/eclipse-workspace/driver/chromedriver.exe" );
-        ChromeOptions options = new ChromeOptions ();
-        options.addArguments ( "headless" );
+    public static void setDriver() throws MalformedURLException {
+        String sauceUserName = System.getenv("SAUCE_USERNAME");
+        String sauceAccessKey = System.getenv("SAUCE_ACCESS_KEY");
 
-        driver = new ChromeDriver (options);
-        driver.manage ().deleteAllCookies ();
+//        System.setProperty ( "webdriver.chrome.driver" , "C:/Users/User/eclipse-workspace/driver/chromedriver.exe" );
+//        ChromeOptions options = new ChromeOptions ();
+//        options.addArguments ( "headless" );
+//
+//        driver = new ChromeDriver (options);
+//        driver.manage ().deleteAllCookies ();
+        DesiredCapabilities capabilities = new DesiredCapabilities ();
+        capabilities.setCapability("username", sauceUserName);
+        capabilities.setCapability("accessKey", sauceAccessKey);
+        capabilities.setCapability("browserName", "Safari");
+        capabilities.setCapability("platform", "macOS 10.13");
+        capabilities.setCapability("version", "11.1");
+//        capabilities.setCapability("name", method.getName());
+
+        driver = new RemoteWebDriver (new URL ("https://ondemand.saucelabs.com:443/wd/hub"), capabilities);
+//        driver.navigate().to("https://paywithbank3d.com/test/");
+
     }
 
     @AfterClass
-    public static void closeDriver() {
-        driver.quit ();
-
+    public static void closeDriver( ) {
+        ((JavascriptExecutor)driver).executeScript("sauce:job-result=" + (result.isSuccess() ? "passed" : "failed"));
+        driver.quit();
     }
 
 }
